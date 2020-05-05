27d2cff34882711a7f6dfdb9dbc58e5c1bfc1056
Inbar Gazit <inbar.gazit@docusign.com>
2020-05-05 14:43:57 -0700

    Adding the eight new code examples to be used by the VS and VS code extensions

---
+++ eg-03-csharp-auth-code-grant-core/Examples/CreatePermissionProfile.cs
@@ -0,0 +1,42 @@
+﻿using DocuSign.eSign.Api;
+using DocuSign.eSign.Client;
+using DocuSign.eSign.Model;
+
+
+namespace eg_03_csharp_auth_code_grant_core.Examples
+{
+    public static class CreatePermissionProfile
+    {
+        /// <summary>
+        /// Creates a permission profile
+        /// </summary>
+        /// <param name="profileName">Profile name</param>
+        /// <param name="accountRoleSettings">Sccount role settings</param>
+        /// <param name="accessToken">Access Token for API call (OAuth)</param>
+        /// <param name="basePath">BasePath for API calls (URI)</param>
+        /// <param name="accountId">The DocuSign Account ID (GUID or short version) for which the APIs call would be made</param>
+        /// <returns>A permission profile</returns>
+        public static PermissionProfile Create(string profileName, AccountRoleSettingsExtension accountRoleSettings, string accessToken, string basePath, string accountId)
+        {
+            // Construct your API headers
+            var config = new Configuration(new ApiClient(basePath));
+            config.AddDefaultHeader("Authorization", "Bearer " + accessToken);
+            AccountsApi accountsApi = new AccountsApi(config);           
+
+            var newPermissionProfile = new PermissionProfile(PermissionProfileName: profileName, Settings: accountRoleSettings);
+
+            // Call the eSignature REST API
+            return accountsApi.CreatePermissionProfile(accountId, newPermissionProfile);
+        }
+
+        /// <summary>
+        /// Temporary subclass for AccountRoleSettings
+        /// This class is needed for now until DCM-3905 is ready
+        /// </summary>
+        public class AccountRoleSettingsExtension : AccountRoleSettings
+        {
+            [System.Runtime.Serialization.DataMember(Name = "signingUIVersion", EmitDefaultValue = false)]
+            public string SigningUiVersion { get; set; }
+        }
+    }
+}
---
+++ eg-03-csharp-auth-code-grant-core/Examples/DeletePermission.cs
@@ -0,0 +1,26 @@
+﻿using DocuSign.eSign.Api;
+using DocuSign.eSign.Client;
+
+namespace eg_03_csharp_auth_code_grant_core.Examples
+{
+    public static class DeletePermission
+    {
+		/// <summary>
+		/// Deletes a permission profile
+		/// </summary>
+		/// <param name="permissionProfileId">Permission profile ID</param>
+		/// <param name="accessToken">Access Token for API call (OAuth)</param>
+		/// <param name="basePath">BasePath for API calls (URI)</param>
+		/// <param name="accountId">The DocuSign Account ID (GUID or short version) for which the APIs call would be made</param>
+		public static void DeletePermissionProfile(string permissionProfileId, string accessToken, string basePath, string accountId)
+		{
+			// Construct your API headers
+			var config = new Configuration(new ApiClient(basePath));
+			config.AddDefaultHeader("Authorization", "Bearer " + accessToken);
+			AccountsApi accountsApi = new AccountsApi(config);
+			// Call the eSignature REST API
+			accountsApi.DeletePermissionProfile(accountId, permissionProfileId);
+		}
+	}
+}
+
